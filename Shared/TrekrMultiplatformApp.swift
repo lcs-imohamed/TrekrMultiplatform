a7b57860601302527e2cf4108d5f994a94484968
Ilana Mohamed <Imohamed@lcs.on.ca>
2021-01-25 14:55:21 -0500

    Initial Commit

---
+++ Shared/TrekrMultiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrMultiplatformApp.swift
+//  Shared
+//
+//  Created by Mohamed, Ilana on 2021-01-25.
+//
+
+import SwiftUI
+
+@main
+struct TrekrMultiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
